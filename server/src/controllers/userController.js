2fbfc3d7dd87fdbfa53cd40600ad8b8fe4ac703c
Bhushan Pandagre <dengercoder790@gmail.com>
2025-01-15 18:44:21 +0530

    controller is running to complete

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,11 +1,27 @@
-import asyncHandler from '../utils/asyncHandler.js';
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiErrors.js"
 
-const registerUser = asyncHandler(async (req,res)=>{
-  res.status(200).json({
-    message:"ok"
-  })
-})
+const registerUser = asyncHandler(async (req, res) => {
+  // res.status(200).json({
+  //   message:"ok"
+  // })
+  // get user details for frontend ..!!
+  // validation- not empty
+  // check if user already exists:username , email
+  //check for images and check for avatar
+  // upload them to cloudinary,avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  //ckeck for user creation
+  // return response
 
+  const { fullname, email, username, password } = req.body;
+  console.log("email", email);
 
+  if(fullname === ""){
+  throw new ApiError
+  }
 
-export {registerUser}
+});
+
+export { registerUser };
